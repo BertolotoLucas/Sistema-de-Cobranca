136ab3ed58bf4c503a3a51f24dffbec7795fc8a5
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-09-20 16:42:19 -0300

    First commit, i've made the first configuration and test the return of ClienteController.

---
+++ service/src/main/java/br/com/bertolotoLucas/sistemaDeCobranca/SistemaDeCobrancaApplication.java
@@ -0,0 +1,12 @@
+package br.com.bertolotoLucas.sistemaDeCobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeCobrancaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeCobrancaApplication.class, args);
+    }
+}
