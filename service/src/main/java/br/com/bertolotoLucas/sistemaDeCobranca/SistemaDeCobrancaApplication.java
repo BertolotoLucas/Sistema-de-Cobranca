13b12a16ade509f3d74d99db1d96f2c28f83324d
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2022-05-30 23:25:48 -0300

    Now starts a service mysql8 when execute

--- service/src/main/java/br/com/bertolotoLucas/sistemaDeCobranca/SistemaDeCobrancaApplication.java
+++ service/src/main/java/br/com/bertolotoLucas/sistemaDeCobranca/SistemaDeCobrancaApplication.java
@@ -1,12 +1,14 @@
 package br.com.bertolotoLucas.sistemaDeCobranca;
 
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaDeCobrancaApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Process process = Runtime.getRuntime().exec("net START MySQL80");
         SpringApplication.run(SistemaDeCobrancaApplication.class, args);
     }
 }
